0c4504b3494b231ca323da38e430f7703b917afb
404 <washesyagws@gmail.com>
2024-11-21 22:43:37 +0700

    Mengganti isi branch develop dengan project baru

---
+++ app/src/main/java/com/example/ones/ui/savings/SavingsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ones.ui.savings
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SavingsViewModel : ViewModel() {
+
+
+}
