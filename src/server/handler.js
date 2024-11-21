fa433f93cfd70e2e3308155062b39db8761acbd6
ferzian <m.ferzian09@gmail.com>
2024-11-21 15:07:12 +0700

    Add Update and Delete Function for Saving

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,29 +1,57 @@
 let savingsData = [];
 
 export const addSaving = (req, res) => {
-    const { name, amount } = req.body;
+    const { name, amount, goal } = req.body;
 
-    if (!name || !amount) {
-        return res.status(400).send({ error: 'Name and amount are required!' });
+    if (!name || !amount || !goal) {
+        return res.status(400).send({ error: 'Name, amount, and goal are required!' });
     }
 
-    const existingSaving = savingsData.find(saving => saving.name === name);
-
-    if (existingSaving) {
-        existingSaving.amount += parseFloat(amount);
-        return res.send({ message: `Amount updated for ${name}!`, data: existingSaving });
-    } else {
-        const newSaving = {
-            id: savingsData.length + 1,
-            name,
-            amount: parseFloat(amount),
-            createdAt: new Date(),
-        }
-        savingsData.push(newSaving);
-        res.status(201).send({ message: 'Saving added successfully!', data: newSaving });
+    const newSaving = {
+        id: savingsData.length + 1,
+        name,
+        amount: parseFloat(amount),
+        goal: parseFloat(goal),
+        status: amount >= goal ? 'completed' : 'in-progress',
+        createdAt: new Date(),
     }
+    savingsData.push(newSaving);
+    res.status(201).send({ message: 'Saving added successfully!', data: newSaving });
 }
 
 export const getSavings = (req, res) => {
     res.send({ data: savingsData });
+}
+
+export const updateSaving = (req, res) => {
+    const { id } = req.params;
+    const { name, amount, goal } = req.body;
+
+    const saving = savingsData.find(saving => saving.id === parseInt(id));
+    console.log(saving);
+
+    if (!saving) {
+        return res.status(404).send({ error: 'Saving not found!' });
+    }
+
+    if (name) saving.name = name;
+    if (amount) saving.amount += parseFloat(amount);;
+    if (goal) saving.goal = parseFloat(goal);
+
+    if (saving.amount >= saving.goal) {
+        saving.status = 'completed';
+    } else {
+        saving.status = 'in-progress';
+    }
+    return res.status(200).send({ message: `Saving amount updated successfully! for ID: ${id}`, data: saving });
+}
+
+export const deleteSaving = (req, res) => {
+    const { id } = req.params;
+    const savingIndex = savingsData.findIndex(saving => saving.id === parseInt(id));
+    if (savingIndex === -1) {
+        return res.status(404).send({ error: 'Saving not found!' });
+    }
+    savingsData.splice(savingIndex, 1);
+    return res.status(200).send({ message: `Saving deleted successfully! for ID: ${id}` });
 }
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import {addSaving, getSavings} from "./handler.js"
+import {addSaving, getSavings, updateSaving, deleteSaving} from "./handler.js"
 
 const routes = express.Router()
 
 routes.post('/savings', addSaving)
 
 routes.get('/savings', getSavings)
+
+routes.put('/savings/:id', updateSaving)
+
+routes.delete('/savings/:id', deleteSaving)
 
 export default routes;
