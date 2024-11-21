ecee0c4ae6249cc0ac737ff279b42daf6a5c032b
ferzian <m.ferzian09@gmail.com>
2024-11-21 13:37:10 +0700

    Creat and Testing Port

---
+++ src/server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express()
+const port = 9000
+
+// testing
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
