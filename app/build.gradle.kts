fde44c61d97b92701869142616f3c137a3fd89da
rizqiau <mochammadrizqiaullia@gmail.com>
2024-11-23 01:36:02 +0700

    Menambahkan SplashActivity, LoginActivity, dan SignupActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
