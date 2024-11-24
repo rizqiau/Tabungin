b76a6a02410e1d7ab80ef767089183ecefbc9b86
rizqiau <mochammadrizqiaullia@gmail.com>
2024-11-25 02:27:46 +0700

    Update Banyak

--- app/src/main/java/com/example/ones/ui/savings/fragments/SavingsFragment.kt
+++ app/src/main/java/com/example/ones/ui/savings/fragments/SavingsFragment.kt
@@ -1,14 +1,14 @@
-package com.example.ones.ui.savings
+package com.example.ones.ui.savings.fragments
 
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.ones.databinding.FragmentSavingsBinding
+import com.example.ones.viewmodel.savings.SavingsViewModel
 
 class SavingsFragment : Fragment() {
 
