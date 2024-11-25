68da51571c7cce51021a188613746be6909ccf5a
ferzian <m.ferzian09@gmail.com>
2024-11-25 08:24:27 +0700

    Secure api

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBu6f9G0OiduxpKDDzZuKKAdBTbQ8sXlIc",
-  authDomain: "tabungin-app-242208.firebaseapp.com",
-  projectId: "tabungin-app-242208",
-  storageBucket: "tabungin-app-242208.firebasestorage.app",
-  messagingSenderId: "66486896293",
-  appId: "1:66486896293:web:70a4d518414054af522f4c",
-  measurementId: "G-EFVLQDTP77"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
-
-// console.log(process.env.FIREBASE_API_KEY);
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
